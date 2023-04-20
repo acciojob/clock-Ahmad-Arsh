0bd50682ff2c43280c21ee1c0b6b1f6b825b717f
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-21 00:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+ function updateClock() {
+      var timer = document.getElementById('timer');
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var seconds = currentTime.getSeconds();
+      var month = currentTime.getMonth() + 1;
+      var day = currentTime.getDate();
+      var year = currentTime.getFullYear();
+      var ampm = hours >= 12 ? 'PM' : 'AM';
+      
+      // Convert hours from 24-hour format to 12-hour format
+      hours = hours % 12;
+      hours = hours ? hours : 12; // Handle midnight (0 hours)
+      
+      // Add leading zeros to single-digit seconds, minutes, and hours
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+      
+      // Display the date and time in the specified format
+      timer.innerHTML = month + "/" + day + "/" + year + " " + hours + ":" + minutes + ":" + seconds + " " + ampm;
+    }
+    
+    window.onload = function() {
+      setInterval(updateClock, 1000); // Update the clock every second
+    };
